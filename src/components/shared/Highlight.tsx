79a866a7be585fb1090ff75669d665a197180fd3
armansinghh <armansingh6692@gmail.com>
2026-08-09 17:16:17 +0530

    feat(Highlight): add Highlighter component

---
+++ src/components/shared/Highlight.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface HighlightProps {
+  children: React.ReactNode;
+  color?: "orange" | "yellow" | "green";
+  tilt?: string;
+}
+
+export function Highlight({ 
+  children, 
+  color = "orange",
+  tilt = "-rotate-1"
+}: HighlightProps) {
+  
+  const colorMap = {
+    orange: "bg-[#d97736]",
+    yellow: "bg-[#e2c140]",
+    green: "bg-[#7ba05b]"
+  };
+
+  return (
+    <span className="relative inline-block whitespace-nowrap mx-0.5 px-0.5">
+      <span className={`absolute inset-0 ${colorMap[color]} ${tilt} -z-10`}></span>
+      <span className="relative z-10">{children}</span>
+    </span>
+  );
+}
